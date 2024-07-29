a04992b896be62dca1d552ff60d0497a64450988
qawatake <38106890+qawatake@users.noreply.github.com>
2024-07-29 19:21:17 +0900

    rm

--- fsel.go
+++ fsel.go
@@ -417,7 +417,6 @@ func (a assignments) add(s *ssa.Store) {
 	}
 }
 
-//nolint:ireturn // cannot return concrete types
 func (a assignments) current(x *ssa.Alloc, instr ssa.Instruction) ssa.Value {
 	stores := a[x]
 	if len(stores) == 0 {
